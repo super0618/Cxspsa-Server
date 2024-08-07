d59bcb23b19d04d623d9b982d881aa472153b78d
Super <newestStar1990@gmail.com>
2024-08-07 07:26:01 -0400

    feat: added jwt token to login api

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 // use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     protected $table = 'cxs_user';
+
+    public function getJWTIdentifier()
+    {
+        // Return the primary key of the user
+        return (string) $this->getKey();
+    }
+
+    // Define the method to return an array of custom claims
+    public function getJWTCustomClaims()
+    {
+        // Return an empty array for default claims
+        return [];
+    }
 }
